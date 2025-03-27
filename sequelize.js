383faa16a9dcd9b37ea7e7d753fc9cd1eb462ea7
GaboNacho <gabrielmunozech@gmail.com>
2025-03-27 02:37:19 -0300

    limpieza codigo

--- sequelize.js
+++ sequelize.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("testTCITGabrielMunoz", "postgres", "123456", {
   host: "localhost",
   dialect: "postgres",
-  logging: false, // Para evitar logs en la consola
+  logging: false
 });
 
 module.exports = sequelize;
